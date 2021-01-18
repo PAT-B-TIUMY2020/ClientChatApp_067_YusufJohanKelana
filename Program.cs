a685b3da97f3f75538881773efa5a7a847d9d266
UcupJake <56532252+UcupJake@users.noreply.github.com>
2021-01-18 12:05:36 +0700

    Client Chat PAT udah siap

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientChatApp_20180140067_Yusuf_Johan_Kelana
+{
+    class Program
+    {
+        // Sebagai context atau channel
+        public class ClientCallback : ServiceReference1.IServiceCallbackCallback
+        {
+            public void pesanKirim(string user, string pesan)
+            {
+                Console.WriteLine("{0}: {1}", user, pesan);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            InstanceContext context = new InstanceContext(new ClientCallback());
+            ServiceReference1.ServiceCallbackClient server = new ServiceReference1.ServiceCallbackClient(context);
+
+            Console.WriteLine("Masukkan username");
+            string nama = Console.ReadLine();
+            server.gabung(nama);
+
+            Console.WriteLine("Kirim pesan");
+            string pesan = Console.ReadLine();
+
+            // Memeriksa apakah pesannya null
+            while (true)
+            {
+                if (!string.IsNullOrEmpty(pesan))
+                    server.kirimPesan(pesan);
+                Console.WriteLine("Kirim Pesan");
+                pesan = Console.ReadLine();
+            }
+        }
+    }
+}
